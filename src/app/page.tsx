b35e5bb8932f24baa949d44174792f893b1ad705
Harsh Sharma <namastecoding@gmailt.com>
2024-06-26 21:40:19 +0530

    fixing dynamic server error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = false
+
 import getCurrentUser from './actions/getCurrentUser';
 import getListings, { IListingsParams } from './actions/getListings';
 import ClientOnly from './components/ClientOnly';
